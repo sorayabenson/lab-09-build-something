4f29e07f3fb4a7a17b5c4de1453909e76f3b43e4
Soraya Benson <soraya.benson@gmail.com>
2021-04-06 16:54:10 -0700

    test for post /ghosts written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,23 @@ describe('lab-09 routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('post /ghosts creates a new ghost and sends it to the database', () => {
+    const newGhost = {
+      name: 'Plant Ghost',
+      img: 'plantghost.png',
+      tagline: 'Is your home a graveyard for house plants? This ghost cares for spirits of all the plants you have killed.',
+      backstory: 'Plant Ghost knew only excessive care for it`s brief, but overwatered life.'
+    }
+
+    return request(app)
+      .post('/ghosts')
+      .send(newGhost)
+      .then((res) => {
+        expect(res.body).toEqual({
+          ...newGhost,
+          id: '1'
+        })
+      })
+  })
 });
